f6c4786cde81398af2a51b97cc2fe6a266f51a2c
Corbin Ball <c.ball@me.com>
2021-10-10 20:12:16 -0500

    used readme gen as starter code to edit

---
+++ utils/generateHtml.js
@@ -0,0 +1,39 @@
+function generateHtml(data){
+    return `<!DOCTYPE html>
+    <html>
+    
+    <head>
+      <title>myTeam</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" type="text/css" href=".dist\style.css">
+    </head>
+    
+    <body>
+     
+    <h1> My Team of dumbasses </h1>
+
+
+    
+    
+    </body>`
+
+    //shit goes here
+
+
+}
+
+function generateEngineer(data) {
+
+
+}
+
+function generateIntern(data) {
+
+
+}
+
+function generateManager(data) {
+
+
+}
