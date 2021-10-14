47c1b18586d8555c40a546510f22167ae9201fc7
Corbin Ball <c.ball@me.com>
2021-10-14 01:16:24 -0500

    started test driven code

---
+++ _tests/Intern.test.js
@@ -0,0 +1,17 @@
+// Importing the Intern class  
+const Intern = require('../lib/Intern');
+
+describe('Intern',()=>{
+    describe('Initialization of Intern',()=>{
+        it.todo('should create an Intern object');
+    });
+    describe('getName method test',()=>{
+        it.todo('should return the name');
+    });
+    describe('getId method test',()=>{
+        it.todo('should return the id');
+    });
+    describe('getEmail method test',()=>{
+        it.todo('should return the email');
+    });
+});
