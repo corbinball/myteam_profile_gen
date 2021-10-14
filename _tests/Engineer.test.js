bf5b2ae16f25896c9956e4f4f5df159db2e583ce
Corbin Ball <c.ball@me.com>
2021-10-14 01:16:00 -0500

    started test driven code

---
+++ _tests/Engineer.test.js
@@ -0,0 +1,17 @@
+// Importing the Engineer class  
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer',()=>{
+    describe('Initialization of Engineer',()=>{
+        it.todo('should create an Engineer object');
+    });
+    describe('getName method test',()=>{
+        it.todo('should return the name');
+    });
+    describe('getId method test',()=>{
+        it.todo('should return the id');
+    });
+    describe('getEmail method test',()=>{
+        it.todo('should return the email');
+    });
+});
