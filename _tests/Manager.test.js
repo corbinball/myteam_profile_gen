5da44063308d9aca08e056e8fcc61e01b4056514
Corbin Ball <c.ball@me.com>
2021-10-14 01:16:43 -0500

    stated test driven code

---
+++ _tests/Manager.test.js
@@ -0,0 +1,17 @@
+// Importing the Manager class  
+const Manager = require('../lib/Manager');
+
+describe('Manager',()=>{
+    describe('Initialization of Manager',()=>{
+        it.todo('should create an Manager object');
+    });
+    describe('getName method test',()=>{
+        it.todo('should return the name');
+    });
+    describe('getId method test',()=>{
+        it.todo('should return the id');
+    });
+    describe('getEmail method test',()=>{
+        it.todo('should return the email');
+    });
+});
