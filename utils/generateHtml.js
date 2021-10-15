9feea2011fd853c192b6e08deb8fd6379a1391c8
Corbin Ball <c.ball@me.com>
2021-10-15 13:05:43 -0500

    working on html and functions

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 
 
 function generateHtml(data){
