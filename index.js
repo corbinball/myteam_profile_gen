e588db8139b0629979befad8652184bd9d092bcd
Corbin Ball <c.ball@me.com>
2021-10-15 13:05:29 -0500

    working on making functions work

--- index.js
+++ index.js
@@ -27,54 +27,87 @@ const employeeQuestions = () => {
         type: "list",
         name: "role",
         message: "What is the role of the Employee?",
-        choices: ["Manager", "Engineer", "Manager"]
+        choices: ["Manager", "Engineer", "Intern"]
     },
-])
+    ]);
+
 };
+
+let role = '${data.role}';
 
 //manager question
 const managerQuestion = () => {
+    if (role === "Manager") {
     return inquirer.prompt({
     type: "input",
     name: "offNum",
     message: "What is your office number?",
 })
 };
+//moreEmployees();
+};
+
+
 //engineer question
 const engineerQuestion = () => {
+    if (role === "Engineer") {
     return inquirer.prompt( {
     type: "input",
     name: "gitHub",
     message: "What is your github username?",
 })
 };
+//moreEmployees();
+};
+
+
 //intern question
 const internQuestion = () => {
+    if (role === "Intern") {
     return inquirer.prompt( {
     type: "input",
     name: "school",
     message: "What is school?"
 })
 };
+//moreEmployees();
+};
+
+
 //add another employee question
 const moreEmployees = () => {
     return inquirer.prompt( {
     type: "confirm",
     name: "anotherOne",
     message: "Do you want to add another employee?",
-    default: false,
+    //default: false,
 })
 };
 
+function addEmployee() {
+    if (moreEmployees === true) {
+        employeeQuestions();
+    }
+
+};
+
+
+
+
 //function to create html file
 function writeToFile(data) {
-    fs.writeFileSync('dist\index.html', generateHtml(data))
+    fs.writeFileSync('.\dist\index.html', generateHtml(data))
 };
 
 
 //function to initialize app
 const init = () => {
     employeeQuestions()
+    // managerQuestion(),
+    // engineerQuestion(),
+    // internQuestion(),
+    // addEmployee()
+
         .then((data) => writeToFile(data))
         .catch((err) => console.error(err));
 };
