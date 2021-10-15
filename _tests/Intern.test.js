d7cb7ffa6d332bca42de186b9a6e41fe665b61fc
Corbin Ball <c.ball@me.com>
2021-10-14 22:47:42 -0500

    created intern test

--- _tests/Intern.test.js
+++ _tests/Intern.test.js
@@ -3,15 +3,28 @@ const Intern = require('../lib/Intern');
 
 describe('Intern',()=>{
     describe('Initialization of Intern',()=>{
-        it.todo('should create an Intern object');
+        it('should create an Intern object',()=>{
+            const intern=new Intern('Corbin',69,'corbin@email.com','fooU');
+
+            expect(intern.name).toEqual('Corbin');
+            expect(intern.id).toEqual(69);
+            expect(intern.email).toEqual('corbin@email.com');
+            expect(intern.school).toEqual('fooU');
+        });
     });
-    describe('getName method test',()=>{
-        it.todo('should return the name');
+    describe('getSchool method test',()=>{
+        it('should return the git hub user name',()=>{
+            const intern=new Intern('Corbin',69,'corbin@email.com','fooU');
+
+            expect(intern.getSchool()).toEqual('fooU');
+        });
     });
-    describe('getId method test',()=>{
-        it.todo('should return the id');
-    });
-    describe('getEmail method test',()=>{
-        it.todo('should return the email');
+    
+    describe('getRole method test',()=>{
+        it('should return the Intern string',()=>{
+            const intern=new Intern('Corbin',69,'corbin@email.com','fooU');
+
+            expect(intern.getRole()).toEqual("Intern");
+        });
     });
 });
