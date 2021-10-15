b089ac701f5b2b2a4c5493db344096802b89b6f2
Corbin Ball <c.ball@me.com>
2021-10-15 10:09:47 -0500

    working with template html

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -11,21 +11,28 @@ function generateHtml(data){
       <title>myTeam</title>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" href=".dist\style.css">
     </head>
     
     <body>
-     
-    <h1> My Team of Workers </h1>
-
-    // <div container goes here for manager>
-
-    // <div container goes here for engineer>
-
-    // <div container goes here for interns>
+        <div class = "container-fluid">
+            <div class = "row d-flex justify-content-center">   
+                <h1 class = "text-center"> My Team of Workers </h1>
+            </div>
+        </div>
 
 
-    
+        <div class = "container">
+            <div class = "row d-flex justify-content-center">
+                <!-- containers for manager, engineer, and interns-->
+            </div>
+        </div>
+
+
+
+
     
     </body>`
 
