af97e3031984fc685416031df1c550f23def6573
Corbin Ball <c.ball@me.com>
2021-10-14 22:42:49 -0500

    created tests for engineer

--- _tests/Engineer.test.js
+++ _tests/Engineer.test.js
@@ -3,15 +3,28 @@ const Engineer = require('../lib/Engineer');
 
 describe('Engineer',()=>{
     describe('Initialization of Engineer',()=>{
-        it.todo('should create an Engineer object');
+        it('should create an Manager object',()=>{
+            const engineer=new Engineer('Corbin',69,'corbin@email.com','fooHub');
+
+            expect(engineer.name).toEqual('Corbin');
+            expect(engineer.id).toEqual(69);
+            expect(engineer.email).toEqual('corbin@email.com');
+            expect(engineer.gitHub).toEqual('fooHub');
+        });
     });
-    describe('getName method test',()=>{
-        it.todo('should return the name');
+    describe('getgitHub method test',()=>{
+        it('should return the git hub user name',()=>{
+            const engineer=new Engineer('Corbin',69,'corbin@email.com','fooHub');
+
+            expect(engineer.getgitHub()).toEqual('fooHub');
+        });
     });
-    describe('getId method test',()=>{
-        it.todo('should return the id');
-    });
-    describe('getEmail method test',()=>{
-        it.todo('should return the email');
+    
+    describe('getRole method test',()=>{
+        it('should return the Engineer string',()=>{
+            const engineer=new Engineer('Corbin',69,'corbin@email.com','fooHub');
+
+            expect(engineer.getRole()).toEqual("Engineer");
+        });
     });
 });
