10d883bcb3ac7e6780eb390ec412283020b97e0b
Corbin Ball <c.ball@me.com>
2021-10-15 21:42:37 -0500

    ugh style bootstrap issues

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -17,7 +17,7 @@ function generateHtml(empEngineer, empManager, empIntern){
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="dist/style.css" />
+      <link rel="stylesheet" href="style.css" />
     </head>
     
     <body>
